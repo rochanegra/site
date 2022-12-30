10d2937879fb314c8a46721aff1dc70d9c166041
Michel <michel@sisefficere.com.br>
2022-12-30 16:17:19 -0300

    Página home pronta, com aviso em breve

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+    <title>Rocha Negra Nº 1 - 150 anos</title>
+</svelte:head>
+
+<main class="h-screen w-screen bg-[#77C1FF]/10">
+    <div class="container flex h-screen justify-center items-center m-auto">
+        <img src="/logo-150anos.webp" alt="" class="w-full lg:w-[40%]">
+        <div class="flex flex-col items-center gap-10">
+            <h1 class="font-agencyfb text-[#CB9727] text-3xl uppercase">Em breve</h1>
+            <form action="https://formsubmit.co/ronymarcon@gmail.com" method="POST" class="flex gap-2">
+                <div class="flex flex-col gap-2">
+                    <input type="text" name="nome" id="" placeholder="Nome" class="p-[0.8rem] px-8 rounded-lg">
+                    <input type="tel" name="telefone" id="" placeholder="Telefone" class="p-[0.8rem] px-8 rounded-lg">
+                </div>
+                <input type="submit" class="p-[1rem] px-8 rounded-lg bg-[#CB9727] text-white cursor-pointer">
+                <input type="hidden" name="_template" value="table">
+            </form>
+        </div>
+    </div>
+</main>
